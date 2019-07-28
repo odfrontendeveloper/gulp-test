2129625bfbf20dafa7b674fe31b0b8e7cf09eee5
odfrontendeveloper <0933931313q@gmail.com>
2019-07-28 12:47:55 +0300

    1

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -9,38 +9,17 @@ const minify = require('gulp-minify');
 const browserSync = require('browser-sync').create();
 const imagemin = require('gulp-imagemin');
 
-const CSS_SRC_HOME = [
-  './src/sass/reset.scss',
-  './src/sass/header.scss',
-  './src/sass/maincontent.scss',
-  './src/sass/howcanwehelp.scss',
-  './src/sass/howcanwehelp.scss',
-  './src/sass/slider.scss',
-  './src/sass/footer.scss'
-];
+const CSS_SRC_HOME = './src/sass/*.scss';
 
-const CSS_SRC_ABOUT = [
-  './src/sass/reset.scss',
-  './src/sass/header.scss',
-  './src/sass/header-about.scss',
-  './src/sass/maincontent.scss',
-  './src/sass/maincontent-about.scss',
-  './src/sass/howcanwehelp.scss',
-  './src/sass/howcanwehelp-about.scss',
-  './src/sass/slider.scss',
-  './src/sass/slider-about.scss',
-  './src/sass/footer.scss'
-];
-
-const JS_SRC = './src/js/*';
+const JS_SRC = './src/js/*.js';
 const CSS_DEST = './build/css';
 const JS_DEST = './build/js'
 
 const LEVEL = 2;
 
 gulp.task('transpilationСSS', function(){
-  return gulp.src(CSS_SRC_ABOUT)
-    .pipe(concat('about.css'))
+  return gulp.src(CSS_SRC_HOME)
+    //.pipe(concat('about.css'))
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 2 versions'],
@@ -50,8 +29,8 @@ gulp.task('transpilationСSS', function(){
 });
 
 gulp.task('transpilationСSSwithMinifacation', function(){
-  return gulp.src(CSS_SRC_ABOUT)
-   .pipe(concat('about.css'))
+  return gulp.src(CSS_SRC)
+   //.pipe(concat('about.css'))
    .pipe(sass().on('error', sass.logError))
    .pipe(autoprefixer({
      overrideBrowserslist: ['last 2 versions'],
@@ -85,35 +64,18 @@ gulp.task('hotReload', function(){
     server: "./"
   });
   gulp.watch("./build").on('change', browserSync.reload);
-  gulp.watch("./about.html").on('change', browserSync.reload);
-
-  CSS_SRC_ABOUT.forEach(function(hrefdir){
-    gulp.watch(hrefdir).on('change', function(){
-        gulp.src(CSS_SRC_ABOUT)
-        .pipe(concat('about.css'))
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-          overrideBrowserslist: ['last 2 versions'],
-          cascade: false
-        }))
-        .pipe(gulp.dest(CSS_DEST));
-    });
-    gulp.watch(CSS_SRC_ABOUT).on('change', browserSync.reload);
+  gulp.watch("./*.html").on('change', browserSync.reload);
+  gulp.watch('./src/sass/**/*.scss').on('change', function(){
+      gulp.src(CSS_SRC_HOME)
+      .pipe(concat('about.css'))
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer({
+        overrideBrowserslist: ['last 2 versions'],
+        cascade: false
+      }))
+      .pipe(gulp.dest(CSS_DEST));
   });
-
-  CSS_SRC_HOME.forEach(function(hrefdir){
-    gulp.watch(hrefdir).on('change', function(){
-        gulp.src(CSS_SRC_HOME)
-        .pipe(concat('about.css'))
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-          overrideBrowserslist: ['last 2 versions'],
-          cascade: false
-        }))
-        .pipe(gulp.dest(CSS_DEST));
-    });
-    gulp.watch(CSS_SRC_HOME).on('change', browserSync.reload);
-  });
+  gulp.watch(CSS_SRC_HOME).on('change', browserSync.reload);
 
 });
 
